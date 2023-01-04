8190d67781baba1cfb1caad89beec194b698eb9f
ccasti26 <ccasti26@calstatela.edu>
2023-01-03 20:58:23 -0800

    Added content to pages, updated CSS and mobile Nav

---
+++ drfa-site/src/pages/DecoratePage.js
@@ -0,0 +1,15 @@
+const DecoratePage = () => {
+    return (
+        <>
+        <main>
+            <h2>Volunteer to decorate the float!</h2>
+            <p>Thank you for your interest in volunteering to decorate the Downey Rose Float!</p>
+            <p>It is truly a  rewarding experience to watch a float you helped decorate proudly turn the corner on Colorado Boulevard on New Year's Day. If this will be your first time decorating a Rose Parade float, you will get a glimpse of the long hours and dedication put into each float, as well as the wonderful memories of working with the community to create a work of art. Newcomers and old-timers alike are welcome to join the decorating effort. Before you do so, please review the rules and guidelines to ensure an enjoyable and safe experience is had by all.</p>
+            <p>Volunteers are needed to add flowers, seeds and other materials that will turn the float into a beautiful work of art.</p>
+            <p>We begin decorating in December during our "Deco Week" which starts on December 26th until the float leaves for the parade the night before the Parade, from 10am to 10pm.</p>
+        </main>
+        </>
+    );
+}
+
+export default DecoratePage;
